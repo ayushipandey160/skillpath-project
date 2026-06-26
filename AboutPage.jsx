447aa1f0d43f3081b5446af9241b9acad822780d
ayushipandey160 <a41190625@gmail.com>
2026-06-26 06:36:48 +0530

    Add files via upload

---
+++ AboutPage.jsx
@@ -0,0 +1,6 @@
+import Footer from '../components/Footer';
+export default function AboutPage(){
+    return <><h1>About SkillPath</h1>
+    <p>SkillPath helps learners improve skills using reusable React components.</p>
+    <Footer/></>
+}
